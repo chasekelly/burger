14fb79a234fbde39f5294bef5b2438d95a7d78a4
Chase Kelly <chaserkelly3211@gmail.com>
2019-04-27 23:45:19 -0700

    Next update

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,12 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3000,
-        user: "root",
-        password: "Maxjc8395!",
-        database: "burgers_db"
-    });
-};
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
 
 connection.connect(function(err) {
     if (err) {
