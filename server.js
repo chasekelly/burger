cba3b97edc5663e718a4c242040d8ac9ce483aa5
Chase Kelly <chaserkelly3211@gmail.com>
2019-04-28 00:08:46 -0700

    push again

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3306;
+var PORT = 3000;
 
 app.use(express.static("public"));
 
